ee48910917b3b51d358232f43c6fd29ec987ab9c
Lucas Amaral <lucasamaralfaria@gmail.com>
2023-10-29 18:34:21 -0300

    Decifracao funcionando

--- src/aes.cpp
+++ src/aes.cpp
@@ -21,12 +21,36 @@ static const array<array<uint8_t, 16>, 16> sBox = {{
     {0x8C, 0xA1, 0x89, 0x0D, 0xBF, 0xE6, 0x42, 0x68, 0x41, 0x99, 0x2D, 0x0F, 0xB0, 0x54, 0xBB, 0x16}
 }};
 
+static const array<array<uint8_t, 16>, 16> invSBox = {
+    { {0x52, 0x09, 0x6A, 0xD5, 0x30, 0x36, 0xA5, 0x38, 0xBF, 0x40, 0xA3, 0x9E, 0x81, 0xF3, 0xD7, 0xFB},
+      {0x7C, 0xE3, 0x39, 0x82, 0x9B, 0x2F, 0xFF, 0x87, 0x34, 0x8E, 0x43, 0x44, 0xC4, 0xDE, 0xE9, 0xCB},
+      {0x54, 0x7B, 0x94, 0x32, 0xA6, 0xC2, 0x23, 0x3D, 0xEE, 0x4C, 0x95, 0x0B, 0x42, 0xFA, 0xC3, 0x4E},
+      {0x08, 0x2E, 0xA1, 0x66, 0x28, 0xD9, 0x24, 0xB2, 0x76, 0x5B, 0xA2, 0x49, 0x6D, 0x8B, 0xD1, 0x25},
+      {0x72, 0xF8, 0xF6, 0x64, 0x86, 0x68, 0x98, 0x16, 0xD4, 0xA4, 0x5C, 0xCC, 0x5D, 0x65, 0xB6, 0x92},
+      {0x6C, 0x70, 0x48, 0x50, 0xFD, 0xED, 0xB9, 0xDA, 0x5E, 0x15, 0x46, 0x57, 0xA7, 0x8D, 0x9D, 0x84},
+      {0x90, 0xD8, 0xAB, 0x00, 0x8C, 0xBC, 0xD3, 0x0A, 0xF7, 0xE4, 0x58, 0x05, 0xB8, 0xB3, 0x45, 0x06},
+      {0xD0, 0x2C, 0x1E, 0x8F, 0xCA, 0x3F, 0x0F, 0x02, 0xC1, 0xAF, 0xBD, 0x03, 0x01, 0x13, 0x8A, 0x6B},
+      {0x3A, 0x91, 0x11, 0x41, 0x4F, 0x67, 0xDC, 0xEA, 0x97, 0xF2, 0xCF, 0xCE, 0xF0, 0xB4, 0xE6, 0x73},
+      {0x96, 0xAC, 0x74, 0x22, 0xE7, 0xAD, 0x35, 0x85, 0xE2, 0xF9, 0x37, 0xE8, 0x1C, 0x75, 0xDF, 0x6E},
+      {0x47, 0xF1, 0x1A, 0x71, 0x1D, 0x29, 0xC5, 0x89, 0x6F, 0xB7, 0x62, 0x0E, 0xAA, 0x18, 0xBE, 0x1B},
+      {0xFC, 0x56, 0x3E, 0x4B, 0xC6, 0xD2, 0x79, 0x20, 0x9A, 0xDB, 0xC0, 0xFE, 0x78, 0xCD, 0x5A, 0xF4},
+      {0x1F, 0xDD, 0xA8, 0x33, 0x88, 0x07, 0xC7, 0x31, 0xB1, 0x12, 0x10, 0x59, 0x27, 0x80, 0xEC, 0x5F},
+      {0x60, 0x51, 0x7F, 0xA9, 0x19, 0xB5, 0x4A, 0x0D, 0x2D, 0xE5, 0x7A, 0x9F, 0x93, 0xC9, 0x9C, 0xEF},
+      {0xA0, 0xE0, 0x3B, 0x4D, 0xAE, 0x2A, 0xF5, 0xB0, 0xC8, 0xEB, 0xBB, 0x3C, 0x83, 0x53, 0x99, 0x61},
+      {0x17, 0x2B, 0x04, 0x7E, 0xBA, 0x77, 0xD6, 0x26, 0xE1, 0x69, 0x14, 0x63, 0x55, 0x21, 0x0C, 0x7D} }
+};
+
 static const array<array<uint8_t, 4>, 4> columnTable = {{
     {2,3,1,1},
     {1,2,3,1},
     {1,1,2,3},
     {3,1,1,2}
 }};
+
+static const array<array<uint8_t, 4>, 4> invColumnTable = { { {0x0E, 0x0B, 0x0D, 0x09},
+                                                           {0x09, 0x0E, 0x0B, 0x0D},
+                                                           {0x0D, 0x09, 0x0E, 0x0B},
+                                                           {0x0B, 0x0D, 0x09, 0x0E} } };
 
 static const array<uint8_t,15> roundConstants = {
     {0x00, 0x01, 0x02, 0x04, 0x08, 0x10, 0x20, 0x40, 0x80, 0x1B, 0x36, 0x6C, 0xD8, 0xAB, 0x4D}
@@ -53,7 +77,7 @@ uint8_t addInGF(uint8_t a, uint8_t b) {
     return a ^ b;
 }
 
-uint8_t operationGF(const array<uint8_t, 4> &elements, const int linha){
+uint8_t operationGF(const array<uint8_t, 4> &elements, const int linha,const array<array<uint8_t, 4>, 4> &columnTable){
     uint8_t sum = 0;
     for(int i = 0;i<elements.size();i++){
         sum = addInGF(sum, multiplyInGF(elements[i], columnTable[linha][i]));
@@ -63,14 +87,14 @@ uint8_t operationGF(const array<uint8_t, 4> &elements, const int linha){
 }
 
 
-uint8_t subByte(const uint8_t byte) {
+uint8_t subByte(const uint8_t byte, const array<array<uint8_t, 16>, 16> &sBox) {
     return sBox[byte >> 4][byte & 0x0F];
 }
 
-void subBytes(array<array<uint8_t, 4>, 4> &block){
+void subBytes(array<array<uint8_t, 4>, 4> &block, const array<array<uint8_t, 16>, 16> &sBox){
     for(int i = 0;i<block.size();i++){
         for(int j = 0;j<block[i].size();j++){
-            block[i][j] = subByte(block[i][j]);
+            block[i][j] = subByte(block[i][j], sBox);
         }
     }
 }
@@ -81,7 +105,7 @@ array<uint8_t,4> g_function(const array<uint8_t,4> &w, int round){
     rotate(g_operation.begin(),g_operation.begin() + 1, g_operation.end());
 
     for(int i = 0;i<g_operation.size();i++){
-        g_operation[i] = subByte(g_operation[i]);
+        g_operation[i] = subByte(g_operation[i], sBox);
     }
     g_operation[0] ^= roundConstants[round]; 
 
@@ -130,7 +154,7 @@ array<array<uint8_t, 4>, 4> keyExpansion(const array<array<uint8_t, 4>, 4> &key,
 
 
 
-void mixColumns(array<array<uint8_t, 4>, 4> &block){ // OK
+void mixColumns(array<array<uint8_t, 4>, 4> &block, const array<array<uint8_t, 4>, 4> &columnTable){ // OK
     for(int coluna = 0;coluna<block.size();coluna++){
         array<uint8_t,4> elementos;
         for(int linha = 0;linha<block[coluna].size();linha++){
@@ -138,7 +162,7 @@ void mixColumns(array<array<uint8_t, 4>, 4> &block){ // OK
         }
         
         for(int linha = 0;linha<block[coluna].size();linha++){
-            uint8_t sum = operationGF(elementos,linha);
+            uint8_t sum = operationGF(elementos,linha,columnTable);
             block[linha][coluna] = sum;
         }
     }
@@ -162,6 +186,12 @@ void shiftRows(array<array<uint8_t, 4>, 4> &block){
 
 }
 
+void inverseShiftRows(array<array<uint8_t, 4>, 4> &block){
+    for(int i = 1;i<block.size();i++){
+        rotate(block[i].rbegin(), block[i].rbegin() + i, block[i].rend());
+    }
+}
+
 
 
 array<array<uint8_t, 4>, 4> aesEncript(array<array<uint8_t, 4>, 4> block, array<array<uint8_t, 4>, 4> key, int n_rounds){
@@ -173,9 +203,9 @@ array<array<uint8_t, 4>, 4> aesEncript(array<array<uint8_t, 4>, 4> block, array<
 
     //1 : n-1 rounds
     for(int i = 1;i<n_rounds;i++){
-        subBytes(block);
+        subBytes(block, sBox);
         shiftRows(block);
-        mixColumns(block);
+        mixColumns(block, columnTable);
 
 
         key = keyExpansion(key,i);
@@ -183,13 +213,41 @@ array<array<uint8_t, 4>, 4> aesEncript(array<array<uint8_t, 4>, 4> block, array<
     }
 
     //Final round
-    subBytes(block);
+    subBytes(block, sBox);
     shiftRows(block);
     key = keyExpansion(key,n_rounds);
     addRoundKey(key,block);
 
     return block;
 }
+
+array<array<uint8_t, 4>, 4> aesDecript(array<array<uint8_t, 4>, 4> block, array<array<uint8_t, 4>, 4> key, int n_rounds){
+    vector<array<array<uint8_t, 4>, 4>> roundKeys;
+    roundKeys.push_back(key);
+
+    for(int i = 1;i<=n_rounds;i++){
+        roundKeys.push_back(keyExpansion(roundKeys[i-1],i));
+    }
+
+    addRoundKey(roundKeys[roundKeys.size()-1],block);
+    roundKeys.pop_back();
+
+    for(int i = 1;i<n_rounds;i++){
+        inverseShiftRows(block);
+        subBytes(block,invSBox);
+        addRoundKey(roundKeys[roundKeys.size()-1],block);
+        mixColumns(block,invColumnTable);
+        roundKeys.pop_back();
+    }
+
+    inverseShiftRows(block);
+    subBytes(block,invSBox);
+    addRoundKey(roundKeys[roundKeys.size()-1],block);
+    roundKeys.pop_back();
+
+    return block;
+}
+
 
 uint64_t generateRandom(){
     random_device rd;
@@ -376,6 +434,25 @@ int main(){
         {0xa8,0x8d,0xa2,0x34}
         }
     };
+
+
+    // array<array<uint8_t, 4>, 4> ciphered_block = {{
+    //     {0x39,0x02,0xdc,0x19},
+    //     {0x25,0xdc,0x11,0x6a},
+    //     {0x84,0x09,0x85,0x0b},
+    //     {0x1d,0xfb,0x97,0x32}
+    // }};
+
+    // array<array<uint8_t, 4>, 4> teste = aesDecript(ciphered_block,key,10);
+
+    // for(int i = 0;i<teste.size();i++){
+    //     for(int j = 0;j<teste[i].size();j++){
+    //         cout << hex << (int) teste[i][j] << " ";
+    //     }
+    //     cout << "\n";
+    // }
+    // cout << "\n";
+
 
 
     int rounds = 10;
